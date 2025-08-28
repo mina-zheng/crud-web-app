9ba4306a04dc3456dd7846d6576b0bf31b610deb
MINA <minazheng505@gmail.com>
2025-08-28 12:46:01 -0400

    set up models and delete

--- crud/models.py
+++ crud/models.py
@@ -1,6 +1,13 @@
 from django.db import models #type: ignore
+from django.contrib.postgres.fields import ArrayField #type: ignore
 
 class User(models.Model):
     name = models.CharField(max_length=100)
     age = models.IntegerField()
+    liked_book_indices = ArrayField(models.IntegerField())
+
+class Books(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.FloatField()
+
 
--- crud/views.py
+++ crud/views.py
@@ -1,4 +1,25 @@
-from django.http import HttpResponse
+from .models import User, Books
+from django.shortcuts import render #type:ignore
 
 def index(request):
-    return HttpResponse("Hello!")
+    books = Books.objects.all()
+    users = User.objects.all()
+
+    if request.method == "POST":
+        if "delete-book" in request.POST:
+            delete(request, "book", Books)
+        elif "delete-user" in request.POST:
+            delete(request, "user", User)
+            
+
+    return render(request, 'crud/index.html', {'books': books,
+                                               'users': users})
+
+
+
+
+def delete(request, db, model):
+    action = "delete-" + db
+    book_name = request.POST.get(action)
+    delete_row = model.objects.filter(name=book_name)
+    delete_row.delete()
